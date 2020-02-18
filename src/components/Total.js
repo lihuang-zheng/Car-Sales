b0b7d3b0522ee9780fd2ee9f33706919e52fc8eb
lihuang-zheng <jerryzheng0131@gmail.com>
2020-02-18 17:27:23 -0600

    added the array for the price and sum for it.

--- src/components/Total.js
+++ src/components/Total.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
 const Total = props => {
+
+  // created array of the prices and added up the sum of the cost.
+  let costArray = props.car.features.map(feature => feature.price)
+  let addedCost = costArray.reduce((total, price) => total + price, 0)
+
   return (
     <div className="content">
-      <h4>Total Amount: ${props.car.price + props.additionalPrice}</h4>
+      <h4>Total Amount: ${props.car.price + addedCost}</h4>
     </div>
   );
 };
